b578da475333735be15d9ced95b6f6199d19005a
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-14 16:22:19 +0300

    Dropdown Web Component - Add Dropdown Item component

---
+++ src/custom-dropdown/custom-dropdown-item.ts
@@ -0,0 +1,45 @@
+import { LitElement, html, css } from "lit";
+
+// Decorators
+import { customElement, property, state,  } from 'lit/decorators.js';
+
+// Directives
+import { classMap } from 'lit/directives/class-map.js';
+
+@customElement('custom-dropdown-item')
+class CustomDropdownItem extends LitElement { 
+  static styles = css`
+  .custom-dropdown-item { 
+    border-bottom: 1px solid black;
+    padding: 10px 0;
+  }
+  .active {
+    font-weight: bold;
+  }`
+
+  @property({ type: Boolean })
+  selected = false;
+
+  @property()
+  customValue = '';
+
+  render() {
+    const classes = {
+      'custom-dropdown-item': true,
+      active: !!this.selected
+    }
+    return html`
+      <li data-custom-value=${this.customValue} 
+        class=${classMap(classes)}
+        aria-selected=${this.selected}>
+        <slot></slot>
+      </li>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLTagElementName {
+    'custom-dropdown-item': CustomDropdownItem
+  }
+}
