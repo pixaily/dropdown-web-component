2d356a8dba36bf926446eea280bfc32bbda6b278
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-14 17:11:20 +0300

    Dropdown Web Component - Add logic to close the dropdown on click ouside of the dropdown

--- src/custom-dropdown/custom-dropdown.ts
+++ src/custom-dropdown/custom-dropdown.ts
@@ -19,28 +19,48 @@ class CustomDropdown extends LitElement {
 
   render() {
     return html`
-    ${this.triggerType === 'link'
-        ? html`<a href="#" role="button" aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</a>`
-        : html`<button type='button' aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</button>`
-    }
-    <ul ?hidden=${!this._isExpanded} @click=${this._clickItemHandler}>
-      ${this.items.map((item: User) => {
-        return html`
-          <custom-dropdown-item key=${item.id} customValue='${item.id}' .selected=${item.selected}>${item.name} ${item.age}</custom-dropdown-item>
-        `
-      })}
-    </ul>
+    <div id="custom-dropdown">
+      ${this.triggerType === 'link'
+          ? html`<a href="#" role="button" aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</a>`
+          : html`<button type='button' aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</button>`
+      }
+      <ul ?hidden=${!this._isExpanded} @click=${this._clickItemHandler}>
+        ${this.items.map((item: User) => {
+          return html`
+            <custom-dropdown-item key=${item.id} customValue='${item.id}' .selected=${item.selected}>${item.name} ${item.age}</custom-dropdown-item>
+          `
+        })}
+      </ul>
+    </div>
     `;
   }
   
   private _toggleDropdownHandler(e: Event) : void {
     e.preventDefault();
+    e.stopPropagation();
+    
     this._isExpanded = !this._isExpanded;
+    
+    const ref = this._closeDropdown.bind(this);
+    if (this._isExpanded) {
+      document.addEventListener('click', ref);
+    } else {
+      document.removeEventListener('click', ref);
+    }
+  }
+  
+  private _closeDropdown(e: Event) {
+    const target = e.target as Element;
+    
+    if (!target.closest('#custom-dropdown')) {
+      this._isExpanded = false;
+    }
   }
 
   private _clickItemHandler(e: Event): void {
+    e.stopPropagation();
     const id = (e.target as Element).getAttribute('key');
-    
+
     this.items.forEach((item: User) => {
       if (item.id !== id) {
         item.selected = false;
