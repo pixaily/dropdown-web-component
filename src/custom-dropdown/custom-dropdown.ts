71139148f2e4ee3039890881497f2948e24d3274
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-14 16:23:25 +0300

    Dropdown Web Component - Add Items private property - Add Dropdown item component - Add logic for show list of items - Add logic for click on item event

--- src/custom-dropdown/custom-dropdown.ts
+++ src/custom-dropdown/custom-dropdown.ts
@@ -1,27 +1,21 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 
-type User = {
-  id: string,
-  name: string,
-  age: Number,
-  selected: Boolean
-}
+import './custom-dropdown-item.ts';
 
 @customElement('custom-dropdown')
 class CustomDropdown extends LitElement {
-  constructor() {
-    super();
-  }
-
   @property()
   buttonLabel = 'Dropdown';
 
   @property()
   triggerType = 'button';
 
+  @property({ type: Array })
+  items: User[] = [];
+
   @state()
-  _isExpanded = false;
+  private _isExpanded = false;
 
   render() {
     return html`
@@ -29,11 +23,41 @@ class CustomDropdown extends LitElement {
         ? html`<a href="#" role="button" aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</a>`
         : html`<button type='button' aria-expanded="${this._isExpanded}" @click="${this._toggleDropdownHandler}">${this.buttonLabel}</button>`
     }
+    <ul ?hidden=${!this._isExpanded} @click=${this._clickItemHandler}>
+      ${this.items.map((item: User) => {
+        return html`
+          <custom-dropdown-item key=${item.id} customValue='${item.id}' .selected=${item.selected}>${item.name} ${item.age}</custom-dropdown-item>
+        `
+      })}
+    </ul>
     `;
   }
   
   private _toggleDropdownHandler(e: Event) : void {
     e.preventDefault();
     this._isExpanded = !this._isExpanded;
+  }
+
+  private _clickItemHandler(e: Event): void {
+    const id = (e.target as Element).getAttribute('key');
+    
+    this.items.forEach((item: User) => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    })
+    const currentItemIdx = this.items.findIndex((item: User) => item.id === id);
+
+    if (currentItemIdx != -1) {
+      this.items[currentItemIdx].selected = !this.items[currentItemIdx].selected
+    }
+
+    this.requestUpdate();
+  }
+}
+
+declare global {
+  interface HTMLTagElementName {
+    'custom-dropdown': CustomDropdown
   }
 }
